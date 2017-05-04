542197625474fafb67808f92fdffa6e05c38a975
Avi Tillu <aatillu@gmail.com>
2017-05-04 09:34:20 +0000

    add update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,26 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
+
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
+
   def show
   end
 
-  # GET /users/new
+
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+  
   def edit
+    @user=User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
+
   def create
     @user = User.new(user_params)
 
@@ -35,22 +33,18 @@ class UsersController < ApplicationController
     
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+
   def update
-    respond_to do |format|
+    @user=User.find(params[:id])
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        flash[:notice]= 'User was successfully updated.'
+        redirect_to articles_path
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'edit'
       end
-    end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+  
   def destroy
     @user.destroy
     respond_to do |format|
