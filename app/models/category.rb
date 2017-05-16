9fba3f6ba9b51d9453fb39c5d16f20628168e6af
Avi Tillu <aatillu@gmail.com>
2017-05-16 12:07:12 +0000

    create UI for adding category to article

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :article_categories
   has_many :articles, through: :article_categories
-  
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
